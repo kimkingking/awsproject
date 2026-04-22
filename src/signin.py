852309dd215858dd2cb887948835ff2186c9778a
jisoo <gnly0428@naver.com>
2026-04-22 08:47:31 +0000

    signin.py 로그 추가

--- src/signin.py
+++ src/signin.py
@@ -24,6 +24,7 @@ def register(
     email: str = Form(...)
 ):
     try:
+        print("REGISTER INPUT:", user_id, password, user_name, phone, addr, email)
         formatted_phone = format_phone_number(phone)
 
         hashed_password = bcrypt.hashpw(
